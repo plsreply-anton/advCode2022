f8530816214977abc33513dc30c99107a86ac572
Anton <antonflinkabo@gmail.com>
2023-10-13 12:59:40 +0200

    setting up new repo

---
+++ 1/main.py
@@ -0,0 +1,27 @@
+import sys
+
+def main():
+    textFile = sys.argv[1]
+    topThree = [0,0,0]
+    with open(textFile, "r") as file:
+        count = 0 
+
+        # Iterate through each line in the file
+        for line in file:
+            
+            # Check if the line is empty (contains only whitespace)
+            if line.strip() == "":
+                if count > topThree[-1]:
+                    topThree[-1] = count
+                count = 0
+                topThree.sort(reverse = True)    
+            else:
+                count += int(line.strip())
+
+    print(f"Elfs with most calories have in total: {topThree[0]+topThree[1]+topThree[2]} cal.")
+
+if __name__ == "__main__":
+
+    main()
+
+    
